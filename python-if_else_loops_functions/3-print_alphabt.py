f6892e2e6cdc6061963b0f457de1b85ceed1369d
Abimael <icemask2612@gmail.com>
2024-05-08 20:33:41 -0400

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 ascii_value_a = ord('a')
 
+output = ""
 
 for i in range(26):
     letter = chr(ascii_value_a + i)
     if letter != 'q' and letter != 'e':
-        print(letter, end='')
+        output += letter
 
-print()
+print(output)
