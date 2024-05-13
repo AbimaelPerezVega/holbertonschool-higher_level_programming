f96dfbd538b7e2d872bd775a2ce052e38957e6bf
Abimael <icemask2612@gmail.com>
2024-05-13 17:01:51 -0400

    huh

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list
+        return my_list[:]
     else:
+        new_list = my_list[:]
         my_list[idx] = element
         return my_list
 
@@ -11,5 +12,3 @@ idx = 3
 new_element = 9
 new_list = replace_in_list(my_list, idx, new_element)
 
-print(new_list)
-print(my_list)
