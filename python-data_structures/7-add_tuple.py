bbfb65696fbe3b64b619ce7c0086d7e81fb1244c
Abimael <icemask2612@gmail.com>
2024-05-13 18:01:00 -0400

    a

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,5 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
                     
     sum_0 = tuple_a[0] + tuple_b[0]
     sum_1 = tuple_a[1] + tuple_b[1]
-                                    
     return (sum_0, sum_1)
