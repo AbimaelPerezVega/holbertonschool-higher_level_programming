d5ba66c8c7595ea828aaec9f6813e70c9870102f
Abimael <icemask2612@gmail.com>
2024-05-27 11:53:16 -0400

    1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    Reads a UTF8 text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
