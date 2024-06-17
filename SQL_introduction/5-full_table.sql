15034bca58bb35d1d6cfc51499a211254cf39fcd
Abimael <icemask2612@gmail.com>
2024-06-17 19:24:15 -0400

    a

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
+-- Print full table description for first_table
 SELECT CONCAT('Table   Create Table') AS 'Table';
 SELECT CONCAT(TABLE_NAME, '     ', CREATE_STATEMENT) AS 'Create Table'
 FROM information_schema.TABLES
