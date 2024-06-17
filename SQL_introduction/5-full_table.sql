096f5edce8e31e3b70e02dff5fa584b2c8b49847
Abimael <icemask2612@gmail.com>
2024-06-17 19:21:17 -0400

    tests 1

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+SELECT CONCAT('Table   Create Table') AS 'Table';
+SELECT CONCAT(TABLE_NAME, '     ', CREATE_STATEMENT) AS 'Create Table'
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'your_database_name'
+AND TABLE_NAME = 'first_table';
