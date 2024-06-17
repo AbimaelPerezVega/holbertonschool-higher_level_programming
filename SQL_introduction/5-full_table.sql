177ffc76ed1eb372fee089ba8e63ec3ebb482314
Abimael <icemask2612@gmail.com>
2024-06-17 19:27:31 -0400

    a

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Print full table description for first_table
-SELECT CONCAT('Table   Create Table') AS 'Table';
-SELECT CONCAT(TABLE_NAME, '     ', CREATE_STATEMENT) AS 'Create Table'
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'your_database_name'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
