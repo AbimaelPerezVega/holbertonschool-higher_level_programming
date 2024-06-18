9e009012a02c12c9922be1b3550db10188e1cfac
Abimael <icemask2612@gmail.com>
2024-06-18 13:15:18 -0400

    permissions

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
--- Grant some privileges to user_0d_1
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-
--- Grant some privileges to user_0d_2
-GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
-
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
