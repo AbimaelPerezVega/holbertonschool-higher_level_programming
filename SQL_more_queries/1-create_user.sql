0008e2a99723941bd9ba66307d7051a3605abdb4
Abimael <icemask2612@gmail.com>
2024-06-18 13:40:32 -0400

    number 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to 'user_0d_1'@'localhost'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
