17f57142ecd5fc0f5bbe3b116ee2953a75043122
Abimael <icemask2612@gmail.com>
2024-06-18 17:12:09 -0400

    a

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- import the database dump
+-- importing data to my server, and the script list all showz contained in it
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
