331191d4e7decc8d927d711bef3abab3e42cddba
Abimael <icemask2612@gmail.com>
2024-06-18 13:46:19 -0400

    removed flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,6 +3,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to 'user_0d_1'@'localhost'
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply the changes
-FLUSH PRIVILEGES;
