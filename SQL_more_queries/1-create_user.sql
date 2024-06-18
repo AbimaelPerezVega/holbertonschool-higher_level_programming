f8597d4111cac108260d0336809494cad33f5046
Abimael <icemask2612@gmail.com>
2024-06-18 13:43:04 -0400

    what

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,17 +1,8 @@
 -- Check if user 'user_0d_1'@'localhost' already exists
-SELECT EXISTS (
-    SELECT 1
-    FROM mysql.user
-    WHERE user = 'user_0d_1' AND host = 'localhost'
-) INTO @user_exists;
-
--- If the user does not exist, create it
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-END IF;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to 'user_0d_1'@'localhost'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
