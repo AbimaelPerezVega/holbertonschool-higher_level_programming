47b0b2dc7f6ff3edf25c58e0b35f6add8bce4ea4
Abimael <icemask2612@gmail.com>
2024-06-18 13:39:15 -0400

    number 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Check if user 'user_0d_1'@'localhost' already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes
+FLUSH PRIVILEGES;
