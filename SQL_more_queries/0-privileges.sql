a8589c2bd30377dec5a389568a0834927bf6f393
Abimael <icemask2612@gmail.com>
2024-06-18 13:10:26 -0400

    users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,11 @@
 -- Ensure user_0d_1 exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 
 -- Ensure user_0d_2 exists
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Grant some privileges to user_0d_2 (for example, SELECT)
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
 
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
