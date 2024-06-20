f760e45c92c85985fb20ccc47c63c8508a16aeba
Abimael <icemask2612@gmail.com>
2024-06-20 18:02:17 -0400

    as if

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,42 +3,45 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 
-import MySQLdb  # type: ignore
+import MySQLdb
 import sys
-# from sqlalchemy import create_engine, Column, Integer, String # type: ignore
-# from sqlalchemy.orm import declarative_base, sessionmaker # type: ignore
-
 
 def main():
     """
     Connects to a MySQL database and lists all states in ascending order by id.
     """
-    # Get MySQL credentials and database name from command line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                         passwd=mysql_password, db=database_name)
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                             passwd=mysql_password, db=database_name)
 
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-    # Execute the SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute the SQL query to select all states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows from the executed query
-    rows = cursor.fetchall()
+        # Fetch all the rows from the executed query
+        rows = cursor.fetchall()
 
-    # Print each row
-    for row in rows:
-        print(row)
+        # Print each row
+        for row in rows:
+            print(row)
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
