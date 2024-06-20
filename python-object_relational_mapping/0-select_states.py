f9df894e78a200fdecb196a234512725410afe2a
Abimael <icemask2612@gmail.com>
2024-06-20 17:00:37 -0400

    fixing 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,43 +3,44 @@
 This script lists all states from the database hbtn_0e_0_usa.
 It takes 3 arguments: mysql username, mysql password, and database name.
 """
-
 import MySQLdb #type: ignore
 import sys
 
-def list_states(username, password, db_name):
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     try:
-        # Connect to the MySQL database
+        # Connect to MySQL database
         db = MySQLdb.connect(
             host="localhost",
-            port=3306,
             user=username,
             passwd=password,
-            db=db_name
+            db=database,
+            port=3306
         )
 
-        # Create a cursor object
+        # Create a cursor object using cursor() method
         cursor = db.cursor()
 
-        # Execute the SQL query
+        # Execute SQL query
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Fetch all the rows
-        rows = cursor.fetchall()
+        # Fetch all rows
+        results = cursor.fetchall()
 
-        # Display the results
-        for row in rows:
+        # Print results as required
+        for row in results:
             print(row)
 
-        # Close the cursor and database connection
-        cursor.close()
+        # Disconnect from server
         db.close()
 
     except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <username> <password> <database>")
-    else:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
