faa7f2de9310b66e7244fc93c58129eb89e205ce
Abimael <icemask2612@gmail.com>
2024-06-20 20:13:30 -0400

    maybe

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,30 +7,33 @@ from sys import argv
 import MySQLdb  # type: ignore
 
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8",
-    )
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8",
+        )
 
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
+        # Create a cursor object to interact with the database
+        cur = db.cursor()
 
-    # Execute the SQL query to select all states
-    # ordered by id in ascending order
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute the SQL query to select all states
+        # ordered by id in ascending order
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows from the executed query
-    states = cur.fetchall()
+        # Fetch all the rows from the executed query
+        states = cur.fetchall()
 
-    # Print each state
-    for state in states:
-        print(state)
+        # Print each state
+        for state in states:
+            print(state)
 
-    # Close the cursor and the database connection
-    cur.close()
-    db.close()
+        # Close the cursor and the database connection
+        cur.close()
+        db.close()
+    except MySQLdb.OperationalError as e:
+        print(f"Error connecting to the database: {e}")
